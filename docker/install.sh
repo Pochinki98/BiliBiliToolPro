d59df023179ff7cbe3de1a1527e3523ab60b6dc8
Noobie III <109138085+Pochinki98@users.noreply.github.com>
2024-05-07 23:58:04 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -18,7 +18,7 @@ echo ' |  _ < (_| | |_| | | |_) | | | | | | (_) | (_) | | '
 echo ' |_| \_\__,_|\__, | |____/|_|_|_| |_|\___/ \___/|_| '
 echo '             |___/                                  '
 
-githubProxy="https://proxy.noobie.top/"
+githubProxy="http://proxy.noobie.top/"
 
 infFileName="ray-inf.sh"
 [ -f $infFileName ] || {
